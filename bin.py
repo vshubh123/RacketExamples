52465be577b94a55288a78b2fe44bdf2580fa1d0
Maroski <michaelkmarona@gmail.com>
2014-06-27 23:53:01 -0400

    Python Update
    Binary converter is updated somewhat

--- bin.py
+++ bin.py
@@ -1,17 +1,24 @@
-def decToBin(m):
-	#print(bin((m // 1)), ". ")
-	temp = m // 1
-	#print(bintemp)
+# Michael Marona
+# bin.py : Given a number, will take the fraction and represent it in a binary format
+
+def printDecToBin(m):
+
+	# Print the whole number
+	print(bin(round(m // 1)), ".", end="")
+
+	# obtain the fractional part of the number
 	temp = m - (m // 1)
 	counter = 1
+
+	# Loop to see which bits should be set
 	while counter != 24:
 		if (temp - 2 ** (counter * -1)) >= 0:
-			print("1 ")
+			print("1", end="")
 			temp = temp - (2 ** (counter * -1))
 		else:
-			print("0 ")
+			print("0", end="")
 		counter = counter + 1
 
-decToBin(1098.8449351194)
-print(bin(1098))
+# printDecToBin(1098.8449351194)
+# print(bin(1098))
 
